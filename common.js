2f8d90696220d6c0021a46b9ab5d03aea606202f
Andrew Stiles <andrewbstiles@gmail.com>
2014-04-30 14:29:25 -0500

    Treat the newtab page as its own site

--- common.js
+++ common.js
@@ -110,5 +110,14 @@ function isLowContrast() {
 }
 
 function isDisallowedUrl(url) {
-  return url.indexOf('chrome') == 0 || url.indexOf('about') == 0;
+  if (url.indexOf('about') == 0) {
+    return true;
+  } else if (url.indexOf('chrome') == 0) {
+    // Special case the "newtab" page, which this extension affects.
+    if (siteFromUrl(url) == 'newtab')
+      return false;
+    else
+      return true;
+  }
+  return false;
 }
