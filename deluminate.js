478d2255478ad01b0f161b5ecfe820934f48954f
Anjou Stiles <anjou.stiles@gmail.com>
2014-08-25 18:14:33 -0500

    Fix fullscreen workaround not always applied
    Handle a race condition where the workaround doesn't get inserted if the
    DOM is not complete.

--- deluminate.js
+++ deluminate.js
@@ -8,15 +8,25 @@ function onExtensionMessage(request) {
       hc += ' hidpi';
     }
     document.documentElement.setAttribute('hc', hc);
-    if (document.body != null &&
-        document.getElementById("deluminate_fullscreen_workaround") == null)
-      document.body.appendChild(fullscreen_workaround);
+    addFullscreenWorkaround();
   } else {
     document.documentElement.removeAttribute('hc');
     workaround_div = document.getElementById("deluminate_fullscreen_workaround");
     if (workaround_div != null)
       workaround_div.remove();
   }
+}
+
+function addFullscreenWorkaround() {
+  /* If the DOM is not loaded, wait before adding the workaround to it.
+   * Otherwise add it immediately. */
+  if (document.body == null) {
+    document.addEventListener('DOMContentLoaded', function() {
+      document.body.appendChild(fullscreen_workaround);
+    });
+  } else if (document.body != null &&
+      document.getElementById("deluminate_fullscreen_workaround") == null)
+    document.body.appendChild(fullscreen_workaround);
 }
 
 function onEvent(evt) {
@@ -51,9 +61,6 @@ function init() {
   chrome.runtime.sendMessage({'init': true, 'url': window.top.document.baseURI},
       onExtensionMessage);
   document.addEventListener('keydown', onEvent, false);
-  window.onload = function() {
-    document.body.appendChild(fullscreen_workaround);
-  }
 }
 
 init();
