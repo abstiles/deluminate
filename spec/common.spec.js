bebb35d69924ea6ff7f2b09d5f005f5d1b9fed38
Andrew Stiles <andrewbstiles@gmail.com>
2017-01-22 16:13:01 -0600

    Add unit tests of getStoredBool

--- spec/common.spec.js
+++ spec/common.spec.js
@@ -1,7 +1,26 @@
-describe("A suite", function() {
-  it("can interact with the local storage", function() {
-    expect(true).toBe(true);
-    localStorage.setItem('test', 'hello world');
-    expect(localStorage.getItem('test')).toBe('hello world');
+require('../common.js');
+
+describe("A stored bool getter", function() {
+  beforeEach(function() {
+    localStorage.setItem('presentTrue', true);
+    localStorage.setItem('presentFalse', false);
+    localStorage.setItem('presentNonBool', 'string, not a bool');
+  });
+
+  it("gets a boolean when present", function() {
+    expect(getStoredBool('presentTrue')).toBe(true);
+    expect(getStoredBool('presentFalse')).toBe(false);
+  });
+
+  it("gets false by default when not present", function() {
+    expect(getStoredBool('notPresent')).toBe(false);
+  });
+
+  it("gets a specified default bool when not present", function() {
+    expect(getStoredBool('notPresent', true)).toBe(true);
+  });
+
+  it("treats non-boolean values as not present", function() {
+    expect(getStoredBool('presentNonBool', true)).toBe(true);
   });
 });
