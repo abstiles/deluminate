aeaf8da813b69679af781df7dc9d90af637557ca
rickytranmer <rwtranmer@gmail.com>
2018-03-17 17:55:54 -0600

    add button to turn on/off textarea readonly, save value to localStorage

--- options.js
+++ options.js
@@ -38,6 +38,21 @@ function loadSettingsDisplay() {
   $('settings').value = JSON.stringify(settings, null, 4);
 }
 
+function editSaveCustomizations() {
+  let editSaveButton = document.getElementById('edit-save');
+  let settingsText = document.getElementById('settings');
+
+  if(editSaveButton.textContent === 'Save site customizations') {
+    editSaveButton.textContent = 'Edit site customizations';
+    settingsText.readOnly = true;
+    localStorage['siteschemes'] = JSON.stringify(JSON.parse(settingsText.value).schemes);
+    localStorage['sitemodifiers'] = JSON.stringify(JSON.parse(settingsText.value).modifiers);
+  } else {
+    editSaveButton.textContent = 'Save site customizations';
+    settingsText.readOnly = false;
+  }
+}
+
 function init() {
   initSettings();
   $('forget').addEventListener('click', onForget, false);
